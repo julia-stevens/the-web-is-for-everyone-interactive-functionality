4d9b250c865ebe8b1319934de7fa9bac5b79feed
julia-stevens <juliastevens02@gmail.com>
2025-03-19 13:07:08 +0100

    #1 disabled bij bookmark

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 import express from 'express'
-
 import { Liquid } from 'liquidjs';
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
-
-
 app.use(express.static('public'))
 
 const engine = new Liquid();
 app.engine('liquid', engine.express());
-
 app.set('views', './views')
 
 // API response links
@@ -82,7 +78,6 @@ app.get("/webinars", async function (request, response) {
   const bookmarksResponseJSON = await bookmarksResponse.json()
 
   const bookmarkedWebinarIds = bookmarksResponseJSON.data.map(bookmark => String(bookmark.text));
-
   const webinarsWithStringIds = webinarResponseJSON.data.map(webinar => ({
     ...webinar,
     id: String(webinar.id)
@@ -179,33 +174,6 @@ app.post("/bookmarks", async function (request, response) {
   })
   response.redirect(303, "/bookmarks")
 })
-
-/* 
-// Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
-app.post(…, async function (request, response) {
-
-  // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
-  console.log(request.body)
-
-  // Via een fetch() naar Directus vullen we nieuwe gegevens in
-
-  // Zie https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch over fetch()
-  // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
-  // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
-  // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
-  await fetch(…, {
-    method: …,
-    body: JSON.stringify(…),
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-    }
-  });
-
-  // Redirect de gebruiker daarna naar een logische volgende stap
-  // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
-  response.redirect(303, …)
-})
-*/
 
 app.set('port', process.env.PORT || 8000)
 
